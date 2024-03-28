9d075ebded676d958787d413b65dc78763119e8e
pluviolithic <e-mckanna@onu.edu>
2024-03-28 16:38:37 -0400

    Fix a few bugs

--- SensorModule.cpp
+++ SensorModule.cpp
@@ -5,25 +5,24 @@ LedControl lc = LedControl(11, 13, 12, 8);  // Initialize LED control pins
 #define ovenTempPin A0       // Define pin for oven temperature sensor
 #define oilTempPin A1        // Define pin for oil temperature sensor
 #define oilPressurePin A2    // Define pin for oil pressure sensor
-#define CS 7                 // Define chip select pin for SPI communication
-
-const uint32_t displayInterval = 100;   // Display update interval in milliseconds
-const uint32_t serialInterval = 900000; // Serial update interval in milliseconds
-
-static uint32_t lastDisplayUpdate = 0;   // Variable to store last display update time
-static uint32_t lastSerialUpdate = 0;    // Variable to store last serial update time
 
 void initializeSensorModule() {
     pinMode(ovenTempPin, INPUT);        // Set oven temperature pin as input
     pinMode(oilTempPin, INPUT);         // Set oil temperature pin as input
     pinMode(oilPressurePin, INPUT);     // Set oil pressure pin as input
-    pinMode(CS, OUTPUT);                 // Set chip select pin as output
+
+    int deviceCount = lc.getDeviceCount();
+    for (int address = 0; address < deviceCount; address++) {
+        lc.shutdown(address, false);
+        lc.setIntensity(address, 8);
+        lc.clearDisplay(address);
+    }
 }
 
 void readSensorData(int dataOut[]) {
     // Read sensor data and map to meaningful values
-    dataOut[0] = map(analogRead(ovenTempPin), 0, 1023, -50, 450);        // Read oven temperature
-    dataOut[1] = map(analogRead(oilTempPin), 0, 1023, -50, 450);         // Read oil temperature
+    dataOut[0] = map(analogRead(ovenTempPin), 0, 1023, -100, 450);        // Read oven temperature
+    dataOut[1] = map(analogRead(oilTempPin), 0, 1023, -100, 450);         // Read oil temperature
     dataOut[2] = map(analogRead(oilPressurePin), 204, 1023, 0, 500);     // Read oil pressure
 }
 
@@ -38,32 +37,17 @@ void writeFourDigitNumber(int offset, int n) {
 }
 
 void sendData(int dataOut[]) {
-    uint32_t currentTime = millis();    // Get current time
-
-    // Send data over serial at regular intervals
-    if (currentTime - lastSerialUpdate >= serialInterval) {
-        lastSerialUpdate = currentTime; // Update last serial update time
-
-        // Print sensor data over serial
-        Serial.print("Oven Temp: ");
-        Serial.println(dataOut[0]);
-        Serial.print("Oil Temp: ");
-        Serial.println(dataOut[1]);
-        Serial.print("Oil Pressure: ");
-        Serial.println(dataOut[2]);
-    }
+    // Print sensor data over serial
+    Serial.print("Oven Temp: ");
+    Serial.println(dataOut[0]);
+    Serial.print("Oil Temp: ");
+    Serial.println(dataOut[1]);
+    Serial.print("Oil Pressure: ");
+    Serial.println(dataOut[2]);
 }
 
 void updateDisplay(int dataOut[]) {
-    uint32_t currentTime = millis();    // Get current time
-
-    // Update LED display at regular intervals
-    if (currentTime - lastDisplayUpdate >= displayInterval) {
-        lastDisplayUpdate = currentTime; // Update last display update time
-
-        // Update LED display with sensor data
-        writeFourDigitNumber(0, dataOut[0]);   // Display oven temperature
-        writeFourDigitNumber(4, dataOut[1]);   // Display oil temperature
-        writeFourDigitNumber(8, dataOut[2]);   // Display oil pressure
-    }
+    writeFourDigitNumber(0, dataOut[0]);   // Display oven temperature
+    writeFourDigitNumber(4, dataOut[1]);   // Display oil temperature
+    writeFourDigitNumber(8, dataOut[2]);   // Display oil pressure
 }
--- sensor-handler.ino
+++ sensor-handler.ino
@@ -1,5 +1,8 @@
 #include "SensorModule.h"
 
+const int interval = 5000;
+long int lastUpdate = -1;
+
 void setup() {
     Serial.begin(9600); // Initialize serial communication
     initializeSensorModule(); // Initialize sensor module
@@ -7,8 +10,13 @@ void setup() {
 
 void loop() {
     int sensorData[3]; // Array to store sensor data
+    long int currentTime = millis();    // Get current time
 
-    readSensorData(sensorData); // Read sensor data
-    sendData(sensorData); // Send sensor data over serial (every 15 minutes)
+    if (currentTime - lastUpdate >= interval) {
+        lastUpdate = currentTime; // Update last serial update time
+        readSensorData(sensorData); // Read sensor data
+        sendData(sensorData); // Send sensor data over serial (every 15 minutes)
+    }
+
     updateDisplay(sensorData); // Update display (every 1 second)
 }
